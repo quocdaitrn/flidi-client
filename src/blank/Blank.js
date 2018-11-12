7272ecdc5b1e940864c32b088657b89fc6f54ef5
An Nguyen <an@fuelcloud.com>
2018-11-12 11:02:15 +0700

    Init project

---
+++ src/blank/Blank.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Blank extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title">
+                    Hello
+                </h1>
+                <div className="desc">
+                    This page for test.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Blank;
