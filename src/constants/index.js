fc6228f7f11248e6bb9f4f28f242b292425ca961
An Nguyen <an@fuelcloud.com>
2019-01-28 16:52:59 +0700

    Change firebase

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,7 @@ export const USERNAME_MAX_LENGTH = 15;
 
 export const EMAIL_MAX_LENGTH = 40;
 
-export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MIN_LENGTH = 6; 
 export const PASSWORD_MAX_LENGTH = 20;
 
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBnmfaj0H3KvSwNfuHIYaFQP35knMs3PCg';
