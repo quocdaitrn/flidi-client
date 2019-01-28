9f94b779493d688d1f2b02d3c00ec4ca93131f2b
An Nguyen <an@fuelcloud.com>
2019-01-28 16:32:03 +0700

    Update config

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,11 +14,3 @@ export const PASSWORD_MAX_LENGTH = 20;
 
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBnmfaj0H3KvSwNfuHIYaFQP35knMs3PCg';
 export const DEFAULT_PROVINCE = 1;
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC8wTy7nK_4QCxz6hD7Ia2EfsIcYA9ryLQ",
-    authDomain: "api-project-814569024038.firebaseapp.com",
-    databaseURL: "https://api-project-814569024038.firebaseio.com",
-    projectId: "api-project-814569024038",
-    storageBucket: "api-project-814569024038.appspot.com",
-    messagingSenderId: "814569024038"
-};
