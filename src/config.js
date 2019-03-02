f1a69abe93f7406c52e8363ddfbed1ba3eb86dbc
An Nguyen <an@fuelcloud.com>
2019-03-02 13:26:07 +0700

    config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC8wTy7nK_4QCxz6hD7Ia2EfsIcYA9ryLQ",
+    authDomain: "api-project-814569024038.firebaseapp.com",
+    databaseURL: "https://api-project-814569024038.firebaseio.com",
+    projectId: "api-project-814569024038",
+    storageBucket: "api-project-814569024038.appspot.com",
+    messagingSenderId: "814569024038"
+}
+export const ADMIN_URL = 'http://admin.flidi.local';
